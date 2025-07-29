63e2045536e8b66f42982b1e4e5e3385e465c608
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-07-29 09:19:47 -0300

    Task banco

--- src/db.js
+++ src/db.js
@@ -1,15 +1,12 @@
+// db.js
 import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // ESSENCIAL para conectar no Railway
+    rejectUnauthorized: false, // importante para Railway
   },
 });
